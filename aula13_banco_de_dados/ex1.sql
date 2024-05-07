52d7d1cc7f781973187d1f0afff30e5a7ad34445
Samuel Ferreira Lopes <96552147+samellop3s@users.noreply.github.com>
2024-05-07 08:35:07 -0300

    Update ex1.sql

--- aula13_banco_de_dados/ex1.sql
+++ aula13_banco_de_dados/ex1.sql
@@ -1,3 +1,7 @@
+DROP TABLE funcionarios
+DROP SEQUENCE sq_0001
+
+
 CREATE TABLE funcionarios
 (
     cd_funcionarios NUMBER(20), 
@@ -15,14 +19,36 @@ START WITH 1
 INCREMENT BY 1 
 CACHE 5;
 
-INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 10049935948, 'Samuel', 'DESENVOLVEDOR', 1, '5000.00');
-INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 26913932588, 'Kamilly', 'RH', 2, '5100.00');
-INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 37393143984, 'Ramon', 'ATENDENTE', 3, '1500.00');
-INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 47036697989, 'Liz', 'GERENTE', 4, '1800.00');
-INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 57996920800, 'Zeli', 'DONA', 5, '5000.00');
+
+
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 10049935948, 'Samuel', 'DESENVOLVEDOR', 1, '8000.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 26913932588, 'Kamilly', 'RH', 2, '6000.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 37393143984, 'Ramon', 'ATENDENTE', 3, '1800.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 47036697989, 'Liz', 'GERENTE', 4, '2000.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 57996920800, 'Zeli', 'DONA', 5, '10000.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 57996920800, 'Arthur', 'CHEFE', 5, '10500.00');
+INSERT INTO funcionarios (cd_funcionarios, cpf, nome, cargo, id_depto, salario) VALUES (sq_0001.nextval, 57996920800, 'Antonio', 'CLIENTE', 5, '10000.00');
 
 SELECT * FROM funcionarios
 
 
-DROP TABLE funcionarios
-DROP SEQUENCE sq_0001
+CREATE OR REPLACE VIEW VW_FUNC AS
+SELECT cd_funcionarios, nome, cargo, id_depto, salario
+FROM funcionarios;
+
+SELECT * FROM VW_FUNC;
+
+
+CREATE OR REPLACE VIEW VW_FUNC2 AS
+SELECT nome, salario
+FROM funcionarios
+WHERE salario > 5000;
+
+SELECT * FROM VW_FUNC2;
+
+CREATE OR REPLACE VIEW VW_FUNC3 AS
+SELECT *
+FROM funcionarios
+WHERE salario >= 1000 AND nome LIKE ('A%');
+
+SELECT * FROM VW_FUNC3;
